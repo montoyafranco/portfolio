e2c3bb6fcd025b8cafa6be9b9c339c496bbd4747
montoyafranco <francomontoya9810@gmail.com>
2022-04-16 01:46:27 -0300

    Dinamic Icons done

--- js/iconos.js
+++ js/iconos.js
@@ -1,15 +1,55 @@
 var iconosArray= [
         {
-          "image": "1"    
+          "image": "../assets/iconos/react_original.png"    
 
         },
         {
-          "image": "2"    
+          "image": "../assets/iconos/sass_icon.png"    
 
         },{
-          "image": "3"    
+          "image": "../assets/iconos/bootstrap_plain.png"    
+
+        },
+        {
+          "image": "../assets/iconos/css_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/redux_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/type_tailwind.png"    
+
+        },
+        {
+          "image": "../assets/iconos/figma_logo.png"    
+
+        },
+        {
+          "image": "../assets/iconos/javascript_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/html_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/mongo_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/git_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/node_icon.png"    
+
+        },
+        {
+          "image": "../assets/iconos/express_logo.png"    
 
         }
+        
     ]
 
- 
